faf187d5062a02b4a30c9fc6b2dda20a6a5ff39a
DanielC78 <3024465830102@ingenieria.usac.edu.gt>
2022-08-10 22:37:38 -0600

    [ FIX ] Validation of data properties

--- model/credits/coursesByStudent.py
+++ model/credits/coursesByStudent.py
@@ -13,9 +13,13 @@ class CoursesByStudent:
     # 6. The state will be int (-1, 0, 1)
 
     coursesData = []
-
-    allowValorsForOptionals = ["1", "0"]
-    allowValorsForState = ["-1", "0", "1"]
+    allowValuesForId = re.compile(r'^[0-9]{3,4}$')
+    allowValuesForName = re.compile(
+        r'^[0-9A-Za-z À-ÿ\u00f1\u00d1]+')
+    allowValuesForPrecourses = re.compile(r'(\d*;?)*')
+    allowValuesForOptionals = re.compile(r'(0|1)')
+    allowValuesForSemesterAndCredits = re.compile(r'^[0-9]{1,2}$')
+    allowValuesForState = re.compile(r'(-1|0|1)')
 
     errorReport = ""
     count = 1
@@ -32,35 +36,53 @@ class CoursesByStudent:
                     verifyCourse = line.split(",")
                     if len(verifyCourse) == 7:
                         # Define a regex to validate just numbers and limite the length of the field to 3-4 characters
-                        if verifyCourse[0] is None or not re.search(r"^[0-9]{3,4}$", verifyCourse[0].strip()):
+                        if verifyCourse[0] is None or not re.fullmatch(self.allowValuesForId, verifyCourse[0].strip()):
                             self.errorReport += "Error en la línea " + \
                                 str(self.count) + \
                                 ": El valor - " + \
                                 verifyCourse[0] + " - no es válido para Id\n"
-                        elif verifyCourse[1] is None or not re.search(r"^[a-zA-Z À-ÿ\u00f1\u00d1]{1,50}$", verifyCourse[1].strip()):
+                        elif verifyCourse[1] is None or not re.match(self.allowValuesForName, verifyCourse[1].strip()):
                             self.errorReport += "Error en la línea " + \
                                 str(self.count) + \
                                 ": El valor - " + \
                                 verifyCourse[1] + \
                                 " - no es válido para Curso\n"
-
-                        elif verifyCourse[3] is None or verifyCourse[3] not in self.allowValorsForOptionals:
+                        elif verifyCourse[2] is None or not re.fullmatch(self.allowValuesForPrecourses, verifyCourse[2].strip()):
                             self.errorReport += "Error en la línea " + \
                                 str(self.count) + \
-                                ": El valor" + \
-                                verifyCourse[3]+"no es válido para Opción\n"
-                        elif verifyCourse[6] is None or verifyCourse[6].strip() not in self.allowValorsForState:
+                                ": El valor - " + \
+                                verifyCourse[2] + \
+                                " - no es válido para Prerrequisitos\n"
+
+                        elif verifyCourse[3] is None or not re.fullmatch(self.allowValuesForOptionals, verifyCourse[3].strip()):
+                            self.errorReport += "Error en la línea " + \
+                                str(self.count) + \
+                                ": El valor " + \
+                                verifyCourse[3]+" no es válido para Opción\n"
+                        elif verifyCourse[4] is None or not re.fullmatch(self.allowValuesForSemesterAndCredits, verifyCourse[4].strip()):
+                            self.errorReport += "Error en la línea " + \
+                                str(self.count) + \
+                                ": El valor - " + \
+                                verifyCourse[4] + \
+                                " - no es válido para Semestre\n"
+                        elif verifyCourse[5] is None or not re.fullmatch(self.allowValuesForSemesterAndCredits, verifyCourse[5].strip()):
+                            self.errorReport += "Error en la línea " + \
+                                str(self.count) + \
+                                ": El valor - " + \
+                                verifyCourse[5] + \
+                                " - no es válido para Créditos\n"
+
+                        elif verifyCourse[6] is None or not re.fullmatch(self.allowValuesForState, verifyCourse[6].strip()):
                             print(type(verifyCourse[6]))
                             self.errorReport += "Error en la línea " + \
                                 str(self.count) + \
                                 ": El valor - " + \
                                 verifyCourse[6] + \
                                 " - no es válido para Estado\n"
-
                         else:
 
                             newCourse = Course(int(verifyCourse[0]), verifyCourse[1], verifyCourse[2],
-                                               verifyCourse[3], verifyCourse[4], verifyCourse[5], verifyCourse[6])
+                                               int(verifyCourse[3]), verifyCourse[4], int(verifyCourse[5]), int(verifyCourse[6]))
                             # Verify if the course exist in the system
                             for course in self.coursesData:
                                 if newCourse.idCourse == course.idCourse:
@@ -72,8 +94,7 @@ class CoursesByStudent:
                             line + " - Error en la cantidad de campos\n"
                 else:
                     self.errorReport += "Error en la línea " + \
-                        str(self.count) + ": " + \
-                        line + " - La línea está vacía\n"
+                        str(self.count) + ": " + " - La línea está vacía\n"
 
                 self.count += 1
             file.close()
